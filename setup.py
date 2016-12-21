c2b03d4708920999fec29b72ad0820514be2ce45
Francisco P. P. Cunha Neto <fcunhaneto@gmail.com>
2016-12-21 11:22:27 -0200

    Criada a distribuição para o PyPi

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+	name			= 'Game of Life',
+	version			= '1.0',
+	py_modules		= ['game_of_life'],
+	author			= 'Francisco Cunha Neto',
+	author_email	= 'fcunhaneto@gmail.com',
+	url				= 'https://github.com/fcunhaneto/game-of-life.git',
+	description		= 'Uma versão feita por pygame de The Game of Life um autômato \
+						celular concebido pelo matemático britânico John Horton Conway'
+	
+	)
